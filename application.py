2dba1c7a6a5294cc9d39808209c004c183874226
Xiang Hu <huxiangtony@gmail.com>
2019-04-18 11:01:57 +0200

    Configurate the main window of the GUI and adjust the paddings.
    Create a method in class to configurate the resize features of the main
    window. Adjust and find the proper paddings between widgets.

--- application.py
+++ application.py
@@ -5,12 +5,13 @@
 @Author: Xiang Hu
 @LastEditors: Xiang Hu
 @Date: 2019-04-10 22:30:18
-@LastEditTime: 2019-04-16 14:01:27
+@LastEditTime: 2019-04-18 10:59:39
 '''
 
 import tkinter as tk
 import tkinter.messagebox as msgb
 from tkinter import ttk
+
 
 class Application(tk.Tk):
     """继承tk中的Tk为父类"""
@@ -21,15 +22,16 @@ class Application(tk.Tk):
         super().__init__()
         self.create_widgets()
         self.grid_widgets()
+        self.resize_config()
         
     def create_widgets(self):
         """创建框架和组件"""
         
         #主框架content
-        self.mainframe = ttk.Frame(self, padding=(4,4,12,12))
+        self.mainframe = ttk.Frame(self, padding=(2,2,3,3))
 
         #添加组件
-        self.frame = ttk.Frame(self.mainframe, borderwidth=5, relief="sunken",\
+        self.frame = ttk.Frame(self.mainframe, borderwidth=2,\
             width=400, height=300,)
         self.search_button = ttk.Button(self.mainframe)
         self.search_button["text"] = "Search"
@@ -56,18 +58,38 @@ class Application(tk.Tk):
         Info_04............................................\n\
         Info_05............................................\n\
         Info_06............................................"
+        self.info_label["relief"] = "sunken"  #下沉
+        self.info_label["anchor"] = "nw"   #文字靠左上角
         
     def grid_widgets(self):
         """放置组件位置"""
         
         self.mainframe.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
         self.frame.grid(column=0, row=0, columnspan=4, rowspan=6, sticky=(tk.N, tk.S, tk.E, tk.W))
-        self.create_button.grid(column=3, row=0, sticky=(tk.N, tk.E), padx=5)
-        self.search_button.grid(column=3, row=1, sticky=(tk.N, tk.E), padx=5)
-        self.edit_button.grid(column=3, row=2, sticky=(tk.N, tk.E), padx=5)
-        self.quit_button.grid(column=3, row=5, sticky=(tk.S, tk.E), padx=5)
-        self.info_label.grid(column=0, row=0, columnspan=3, rowspan=6, sticky=(tk.N, tk.S, tk.E, tk.W), padx=5, pady=5)
+        self.create_button.grid(column=3, row=0, sticky=(tk.N))
+        self.search_button.grid(column=3, row=1, sticky=(tk.N))
+        self.edit_button.grid(column=3, row=2, sticky=(tk.N))
+        self.quit_button.grid(column=3, row=5, sticky=(tk.S))
+        self.info_label.grid(column=0, row=0, columnspan=3, rowspan=6, sticky=(tk.N, tk.S, tk.E, tk.W))
 
+    def resize_config(self):
+        """设置resize的属性，即跟随窗口扩大的系数"""
+        
+        #root
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        #mainframe(content)
+        self.mainframe.columnconfigure(0, weight=3)
+        self.mainframe.columnconfigure(1, weight=3)
+        self.mainframe.columnconfigure(2, weight=3)
+        self.mainframe.columnconfigure(3, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
+        self.mainframe.rowconfigure(1, weight=1)
+        self.mainframe.rowconfigure(2, weight=1)
+        self.mainframe.rowconfigure(3, weight=3)  #“Quit”按钮上面的空间
+        self.mainframe.rowconfigure(4, weight=3)  #“Quit”按钮上面的空间
+        self.mainframe.rowconfigure(5, weight=1)
+    
     def create(self):
         """新建数据库，用来保存新用户的数据"""
 
